80aa442222a4e54fe4cade8d4d47d84bba905944
Soumyajyoti Karmakar <ksoumyajyoti@gmail.com>
2021-11-15 21:55:50 +0530

    Create Length of a string.cpp

---
+++ Length of a string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int count=0;
+    string str;
+    string temp="";
+    getline(cin,str);
+    // for(int i=0;i<str.size();i++){
+    //     count++;
+    // }
+    // cout<<count;
+    cout<<str.length();
+    return 0;
+}
